9b87f1a091f506a7cd19f33d13ba029a14e79108
Ian Callahan <code@greatfortress.com>
2025-06-07 14:01:28 -0400

    filament user

--- app/Models/User.php
+++ app/Models/User.php
@@ -7,11 +7,17 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
+use Filament\Models\Contracts\FilamentUser;
 
-class User extends Authenticatable
+class User extends Authenticatable implements FilamentUser
 {
     /** @use HasFactory<\Database\Factories\UserFactory> */
     use HasFactory, Notifiable, HasApiTokens;
+
+    public function canAccessPanel(Panel $panel): bool
+    {
+        return $this->email === 'ian_callahan@harvard.edu';
+    }
 
     /**
      * The attributes that are mass assignable.
